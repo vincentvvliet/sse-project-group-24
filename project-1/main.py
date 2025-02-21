02ed910faf513545734d12c08f657f45f3d2bd21
Vincent van Vliet <b.v.vanvliet@student.tudelft.nl>
2025-02-21 15:10:11 +0100

    Implement basic fibonacci and upload experiment.sh

--- project-1/main.py
+++ project-1/main.py
@@ -1,1 +1,12 @@
-#TODO
+def fibonacci(n):
+    if n < 0:
+        print("Incorrect input")
+
+    elif n == 0:
+        return 0
+
+    elif n == 1 or n == 2:
+        return 1
+
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
